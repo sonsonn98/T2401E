1de9db07ef079dbe265cf7ae84775b67ceabd866
tuananh21 <chutuananh.vnu@gmail.com>
2024-07-13 15:40:31 +0700

    Tuan Anh update

--- Sem1_Project/Tuan Anh/css,js/slide.js
+++ Sem1_Project/Tuan Anh/css,js/slide.js
@@ -1,30 +1,35 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+function initSlideshows() {
+  let slideshows = document.getElementsByClassName("slideshow-container");
+  for (let i = 0; i < slideshows.length; i++) {
+      slideshows[i].slideIndex = 1;
+      showSlides(1, slideshows[i].id);
+  }
 }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+function plusSlides(n, slideshowId) {
+  let slideshow = document.getElementById(slideshowId);
+  showSlides(slideshow.slideIndex += n, slideshowId);
 }
 
-function showSlides(n) {
+function currentSlide(n, slideshowId) {
+  showSlides(n, slideshowId);
+}
+
+function showSlides(n, slideshowId) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  let slideshow = document.getElementById(slideshowId);
+  let slides = slideshow.getElementsByClassName("mySlides");
+  let dots = document.querySelectorAll(`#${slideshowId} ~ .dot-btn .dot`);
+  if (n > slides.length) { slideshow.slideIndex = 1 }
+  if (n < 1) { slideshow.slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slideshow.slideIndex - 1].style.display = "block";
+  dots[slideshow.slideIndex - 1].className += " active";
 }
+
+document.addEventListener("DOMContentLoaded", initSlideshows);
